acee74fe109edfbb441980470ba1aac0b2598669
Paradigm Extra <135075234+paradigmextra@users.noreply.github.com>
2023-07-03 19:59:46 +0200

    updated updated

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
  * set_string - sets the value of a pointer to a char
- * @s: pointer to change
- * @to: string to change pointer to
- *
+ * @s: pointer to pointer
+ * @to: pointer to char
  * Return: void
  */
 void set_string(char **s, char *to)
